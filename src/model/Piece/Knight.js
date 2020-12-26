b092eba6a61275cc387f550649352c18583fafdc
clust3rr <thg.santos@outlook.com>
2020-12-26 19:13:07 -0300

    bug fix

--- src/model/Piece/Knight.js
+++ src/model/Piece/Knight.js
@@ -23,6 +23,7 @@ export default function (color, position) {
       ((!board[sourcePosition.i + 2][sourcePosition.j - 1] && sourcePosition.i + 2 === targetPosition.i && sourcePosition.j - 1 === targetPosition.j) ||
         (board[sourcePosition.i + 2][sourcePosition.j - 1] && sourcePosition.i + 2 === targetPosition.i && sourcePosition.j - 1 === targetPosition.j &&
           board[sourcePosition.i + 2][sourcePosition.j - 1].color !== this.color)) ||
+
       //down
       ((!board[sourcePosition.i + 1][sourcePosition.j - 2] && sourcePosition.i + 1 === targetPosition.i && sourcePosition.j - 2 === targetPosition.j) ||
         (board[sourcePosition.i + 1][sourcePosition.j - 2] && sourcePosition.i + 1 === targetPosition.i && sourcePosition.j - 2 === targetPosition.j &&
@@ -37,9 +38,9 @@ export default function (color, position) {
         (board[sourcePosition.i - 2][sourcePosition.j - 1] && sourcePosition.i - 2 === targetPosition.i && sourcePosition.j - 1 === targetPosition.j &&
           board[sourcePosition.i - 2][sourcePosition.j - 1].color !== this.color)) ||
 
-      ((!board[sourcePosition.i - 2][sourcePosition.j + 2] && sourcePosition.i - 2 === targetPosition.i && sourcePosition.j + 2 === targetPosition.j) ||
-        (board[sourcePosition.i - 2][sourcePosition.j + 2] && sourcePosition.i - 2 === targetPosition.i && sourcePosition.j + 2 === targetPosition.j &&
-          board[sourcePosition.i - 2][sourcePosition.j + 2].color !== this.color))
+      ((!board[sourcePosition.i - 2][sourcePosition.j + 1] && sourcePosition.i - 2 === targetPosition.i && sourcePosition.j + 1 === targetPosition.j) ||
+        (board[sourcePosition.i - 2][sourcePosition.j + 1] && sourcePosition.i - 2 === targetPosition.i && sourcePosition.j + 1 === targetPosition.j &&
+          board[sourcePosition.i - 2][sourcePosition.j + 1].color !== this.color))
 
     ) {
       return { status: true }
